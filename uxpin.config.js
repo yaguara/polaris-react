54af5daca83f47ae0f0b98f431b70c40bbb37c63
Ben Scott <ben.scott@shopify.com>
2019-05-09 23:22:15 +0200

    Generate uxpin categories based on readme categories
    Still a bit WIP as I need a way to work out how to handle subcomponents
    in a way that isn't totally annoying

--- uxpin.config.js
+++ uxpin.config.js
@@ -1,36 +1,67 @@
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+const grayMatter = require('gray-matter');
+
+const blockList = [
+  // Throws "TypeError: Cannot read property 'kind' of undefined" error
+  'ChoiceList',
+
+  // Thows "Error: No component found!"
+  'ContextualSaveBar',
+  'InlineError',
+  'Tag',
+  'TextField',
+  'Icon',
+  'ProgressBar',
+  'Spinner',
+  'Scrollable',
+  'Pagination',
+
+  // TODO Needs presets // subcomponent stuff
+  'Card',
+];
+
+const readmeMetadatas = glob
+  .sync(path.resolve(__dirname, 'src/components/*/README.md'))
+  .filter((readmePath) =>
+    fs.existsSync(readmePath.replace(/README\.md$/, 'index.ts')),
+  )
+  .map((readme) => ({
+    pathPart: path.dirname(
+      path.relative(`${__dirname}/src/components`, readme),
+    ),
+    category: grayMatter(fs.readFileSync(readme)).data.category,
+  }))
+  .reduce((memo, component) => {
+    if (!memo[component.category]) {
+      memo[component.category] = [];
+    }
+
+    if (!blockList.includes(component.pathPart)) {
+      memo[component.category].push(
+        `src/components/${component.pathPart}/${component.pathPart}.tsx`,
+      );
+    }
+    return memo;
+  }, {});
+
+const uxPinCategories = Object.entries(readmeMetadatas).reduce(
+  (memo, [name, include]) => {
+    memo.push({name, include});
+    return memo;
+  },
+  [],
+);
+
+// TODO bring back sub components
+// 'src/components/Card/components/Header/Header.tsx',
+// 'src/components/Card/components/Section/Section.tsx',
+
+console.log(uxPinCategories);
 module.exports = {
   name: 'Shopify Polaris',
   components: {
-    categories: [
-      {
-        name: 'Actions',
-        include: [
-          'src/components/Button/Button.tsx',
-          'src/components/Link/Link.tsx',
-        ],
-      },
-      {
-        name: 'Structure',
-        include: [
-          'src/components/Card/Card.tsx',
-          'src/components/Card/components/Header/Header.tsx',
-          'src/components/Card/components/Section/Section.tsx',
-        ],
-      },
-      {
-        name: 'Images and Icons',
-        include: [
-          'src/components/Avatar/Avatar.tsx',
-          'src/components/Badge/Badge.tsx',
-        ],
-      },
-      {
-        name: 'Forms',
-        include: [
-          'src/components/Checkbox/Checkbox.tsx',
-          'src/components/ChoiceList/ChoiceList.tsx',
-        ],
-      },
-    ],
+    categories: uxPinCategories,
   },
 };
