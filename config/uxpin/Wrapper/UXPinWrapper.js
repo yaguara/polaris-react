fa9ec0c2c02175ed8a4989dac034da901801c907
Kaelig <kaelig.deloumeauprigent@shopify.com>
2019-09-24 12:29:07 -0700

    Update uxpin, add wrapper to mimic text properties

--- config/uxpin/Wrapper/UXPinWrapper.js
+++ config/uxpin/Wrapper/UXPinWrapper.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import tokens from '@shopify/polaris-tokens';
 
 // eslint-disable-next-line shopify/strict-component-boundaries
 import AppProvider from '../../../src/components/AppProvider';
@@ -19,7 +20,16 @@ const theme = {
       'https://cdn.shopify.com/s/files/1/0446/6937/files/jaded-pixel-logo-gray.svg?6215648040070010999',
   },
 };
+
 // eslint-disable-next-line react/prop-types
 export default function UXPinWrapper({children}) {
-  return React.createElement(AppProvider, {theme, i18n: {}}, children);
+  return React.createElement('div', {
+    // Simulate default 'body' styles
+    style: {
+      color: tokens.colorInk,
+      fontFamily: tokens.fontStackBase,
+      webkitFontSmoothing: 'antialiased',
+      textRendering: 'optimizeLegibility',
+    },
+  }, [React.createElement(AppProvider, {theme, i18n: {}}, children)]);
 }
