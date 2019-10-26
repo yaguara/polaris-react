4370986180441f924f840cf7a027578057789770
Sam Bostock <sam.bostock@shopify.com>
2019-10-25 22:09:02 -0400

    fix hook name import

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -3,7 +3,7 @@ import {addEventListener} from '@shopify/javascript-utilities/events';
 import {CircleCancelMinor} from '@shopify/polaris-icons';
 import {VisuallyHidden} from '../VisuallyHidden';
 import {classNames, variationName} from '../../utilities/css';
-import {useHiddenNotEmpty} from '../../utilities/a11y';
+import {useHiddenNotBlank} from '../../utilities/a11y';
 import {useI18n} from '../../utilities/i18n';
 import {useUniqueId} from '../../utilities/unique-id';
 import {Labelled, Action, helpTextID, labelID} from '../Labelled';
